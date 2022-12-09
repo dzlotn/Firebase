f49e54913ca3a249a34010bfdd16ccf3cfb4d2ce
Dzlotnick <daniel.zlotnick5@gmail.com>
2022-12-09 12:08:03 -0500

    pull firebase files to computer

---
+++ Unit 3 Database Programming/Firebase/js/home.js
@@ -0,0 +1,18 @@
+// ----------------- Page Loaded After User Sign-in -------------------------//
+
+// ----------------- Firebase Setup & Initialization ------------------------//
+// Import the functions you need from the SDKs you need
+
+
+
+
+// ---------------------// Get reference values -----------------------------
+
+
+
+// ----------------------- Get User's Name'Name ------------------------------
+
+
+
+
+// --------------------------- Home Page Loading ----------------------------
---
+++ Unit 3 Database Programming/Firebase/js/register.js
@@ -0,0 +1,20 @@
+// This JS file is for registering a new app user ---------------------------//
+
+// ----------------- Firebase Setup & Initialization ------------------------//
+
+
+// ---------------- Register New Uswer --------------------------------//
+
+
+
+// --------------- Check for null, empty ("") or all spaces only ------------//
+function isEmptyorSpaces(str){
+  return str === null || str.match(/^ *$/) !== null
+}
+
+// ---------------------- Validate Registration Data -----------------------//
+
+
+// --------------- Password Encryption -------------------------------------//
+
+
