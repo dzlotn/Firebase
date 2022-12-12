abb916668ccc13918f7b7f6621cf1ff535b52f66
Dzlotnick <daniel.zlotnick5@gmail.com>
2022-12-12 14:18:47 -0500

    completed more on the home.js file

--- Unit 3 Database Programming/Firebase/js/home.js
+++ Unit 3 Database Programming/Firebase/js/home.js
@@ -7,7 +7,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+  import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
   import { getDatabase, ref, set,update,child,get,remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
   // Your web app's Firebase configuration
@@ -30,9 +30,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 
 
 // ---------------------// Get reference values -----------------------------
-let userLink = document.getElementsByID('userLink') //user name for navbar
-let signOutLink = document.getElementsByID('signOut'); //sign out link
-let welcome = document.getElementsByID('welcome') //welcome header
+let userLink = document.getElementById('userLink') //user name for navbar
+let signOutLink = document.getElementById('signOut'); //sign out link
+let welcome = document.getElementById('welcome') //welcome header
 let currentUser = null;//initialize currentUser to null
 
 
@@ -46,13 +46,26 @@ function getUserName(){
 
   //Grab user information passed form signIn.js
   if(keepLoggedIn == "yes"){
-    currentUser = JSON.parse(localStorage,getItem('user'));
+    currentUser = JSON.parse(localStorage.getItem('user'));
 
   }
   else{
-    currentUser = JSON.parse(sessionStorage,getItem('user'));
+    currentUser = JSON.parse(sessionStorage.getItem('user'));
    
   }
+}
+function signOutUser(){
+  sessionStorage.removeItem('user')   //clear session storage
+  localStorage.removeItem('user')     //clear local storage
+  localStorage.removeItem('keepLoggedIn'); 
+  signOut(auth).then(()=>{
+
+    //sign-out successfull
+  }).catch((error) =>{
+    //error ocured
+  })
+  window.location = 'home.html'
+
 
 }
 
@@ -62,13 +75,60 @@ function getUserName(){
 
 
 // ------------------------Set (insert) data into FRD ------------------------
-
+function setData(userID,year,month,day,temperature){
+//must use brackets around variable name to use it as a key dumbo
+    set(ref(db,'users/'+userID+'/data/'+year+'/'+month),{
+      [day]: temperature
+    })
+    .then(()=>{
+      alert("Data stored successfully.")
+    })
+    .catch((error)=>{
+      alert("There was an error. Error: " +error)
+    });
+}
 
 // -------------------------Update data in database --------------------------
-
+function updateData(userID,year,month,day,temperature){
+  //must use brackets around variable name to use it as a key dumbo
+      update(ref(db,'users/'+userID+'/data/'+year+'/'+month),{
+        [day]: temperature
+      })
+      .then(()=>{
+        alert("Data updated successfully.")
+      })
+      .catch((error)=>{
+        alert("There was an error. Error: " +error)
+      });
+  }
 
 // ----------------------Get a datum from FRD (single data point)---------------
+function getData(userID,year,month,day){
+  let yearVal = document.getElementById('yearVal');
+  let monthVal = document.getElementById('monthVal');
+  let dayVal = document.getElementById('dayVal');
+  let tempVal = document.getElementById('tempVal');
+  const dbref = ref(db); //firebase parameter required for get
 
+  //provide the path through the nodes to the data
+  get(child(dbref,'users/'+userID + '/data/' + year + '/' +month )).then((snapshot)=>{
+    if(snapshot.exists()){
+      yearVal.textContent = year;
+      monthVal.textContent = month;
+      dayVal.textContent = day;
+      //to get specific value from a key: snapshot.val()[key]
+      tempVal.textContent = snapshot.val()[day];
+    }
+    else{
+      alert('no data found.')
+    }
+  })
+  .catch((error)=>{
+    alert('unsuccessful, error: '+error)
+  })
+
+  
+}
 
 // ---------------------------Get a month's data set --------------------------
 // Must be an async function because you need to get all the data from FRD
@@ -85,18 +145,71 @@ function getUserName(){
 
 // --------------------------- Home Page Loading -----------------------------
 
-  // ------------------------- Set Welcome Message -------------------------
+window.onload = function(){
+// ------------------------- Set Welcome Message -------------------------
+  getUserName();
+  if(currentUser == null){
+  userLink.innerText = "Create New Account";
+  userLink.classList.replace("nav-link","btn");
+  userLink.classList.add("btn-primary")
+  userLink.href = "register.html";
+
+  signOutLink.innerText = "Sign In"
+  signOutLink.classList.replace("nav-link","btn")
+  signOutLink.classList.add("btn-success");
+  signOutLink.href = "signIn.html";
+  }
+  else{
+      userLink.innerText = currentUser.firstname;
+      welcome.innerText = "Welcome "+currentUser.firstname;
+      userLink.classList.replace("btn","nav_link");
+      userLink.classList.add("btn-primary")
+      userLink.href = "#";
+
+      signOutLink.innerText = "Sign Out"
+      signOutLink.classList.replace("btn","nav_link")
+      signOutLink.classList.add("btn-success");
+      document.getElementById('signOut').onclick = function(){
+        signOutUser();
+      }
+  }
+  
 
   
   // Get, Set, Update, Delete Sharkriver Temp. Data in FRD
   // Set (Insert) data function call
+  document.getElementById('set').onclick = function(){
+    const year = document.getElementById('year').value;
+    const month = document.getElementById('month').value;
+    const day = document.getElementById('day').value;
+    const temperature = document.getElementById('temperature').value;
+    const userID = currentUser.uid;
+    setData(userID,year,month,day,temperature);
+  };
   
+ 
+  
+ 
 
   // Update data function call
-  
+  document.getElementById('update').onclick = function(){
+    const year = document.getElementById('year').value;
+    const month = document.getElementById('month').value;
+    const day = document.getElementById('day').value;
+    const temperature = document.getElementById('temperature').value;
+    const userID = currentUser.uid;
+    updateData(userID,year,month,day,temperature);
+  };
 
   // Get a datum function call
-  
+  document.getElementById('get').onclick = function(){
+    const year = document.getElementById('getYear').value;
+    const month = document.getElementById('getMonth').value;
+    const day = document.getElementById('getDay').value;
+    const userID = currentUser.uid;
+    getData(userID,year,month,day);
+  };
+}
 
   // Get a data set function call
   
