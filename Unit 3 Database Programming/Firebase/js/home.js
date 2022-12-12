324f8bd811e5cf648dc86c7d517b7e6258efcdc7
Dzlotnick <daniel.zlotnick5@gmail.com>
2022-12-12 12:17:43 -0500

    overwrote the files

--- Unit 3 Database Programming/Firebase/js/home.js
+++ Unit 3 Database Programming/Firebase/js/home.js
@@ -1,8 +1,8 @@
 // ----------------- Page Loaded After User Sign-in -------------------------//
 
 // ----------------- Firebase Setup & Initialization ------------------------//
-// Import the functions you need from the SDKs you need
 
+// Import the functions you need from the SDKs you need
 
 
 
@@ -13,6 +13,51 @@
 // ----------------------- Get User's Name'Name ------------------------------
 
 
+// Sign-out function that will remove user info from local/session storage and
+// sign-out from FRD
 
 
-// --------------------------- Home Page Loading ----------------------------
+
+// ------------------------Set (insert) data into FRD ------------------------
+
+
+// -------------------------Update data in database --------------------------
+
+
+// ----------------------Get a datum from FRD (single data point)---------------
+
+
+// ---------------------------Get a month's data set --------------------------
+// Must be an async function because you need to get all the data from FRD
+// before you can process it for a table or graph
+
+
+// Add a item to the table of data
+
+
+
+// -------------------------Delete a day's data from FRD ---------------------
+
+
+
+// --------------------------- Home Page Loading -----------------------------
+
+  // ------------------------- Set Welcome Message -------------------------
+
+  
+  // Get, Set, Update, Delete Sharkriver Temp. Data in FRD
+  // Set (Insert) data function call
+  
+
+  // Update data function call
+  
+
+  // Get a datum function call
+  
+
+  // Get a data set function call
+  
+
+  // Delete a single day's data function call
+  
+
